68ad4f9377f43309caa28b9fa1722c2070e5654d
NG0038094 <NG0038094@INPUHJPC10947.ATT.TechMahindra.com>
2019-02-28 12:49:05 +0530

    basic commit example

---
+++ src/main/java/com/nag/example/SpringdAppexampleApplication.java
@@ -0,0 +1,13 @@
+package com.nag.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringdAppexampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringdAppexampleApplication.class, args);
+	}
+
+}
